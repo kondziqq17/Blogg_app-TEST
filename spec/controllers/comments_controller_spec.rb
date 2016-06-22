29ae2519a2312d266bfb3dfe7bcefa7f8d59cb70
Konrad Pason <konrad.pason17@gmail.com>
2016-06-22 13:19:24 +0200

    Force signin or signout before posting comments

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
+require 'support/macros'
 
 RSpec.describe CommentsController, type: :controller do
+	describe "POST and #create" do
+		before do
+			@john = User.create(email: "john@example.com", password: "password")
+		end
 
+		context "signed in user" do
+			it "can create comment" do
+				login_user @john
+				article = Article.create(title: "First article", body: "Body of first artcile", user: @john )
+				post :create, comment: {body: "Awesome post"}, 
+				article_id: article.id
+				expect(flash[:success]).to eq("Comment has been created")
+		end
+	end
+
+		context "non-signed id user" do
+			it "is redirected to sign in page" do
+				login_user nil
+				article = Article.create(title: "First article", body: "Body of first artcile", user: @john )
+				post :create, comment: {body: "Awesome post"},
+				article_id: article.id
+				expect(response).to redirect_to new_user_session_path
+			end
+		end
+	end
 end
