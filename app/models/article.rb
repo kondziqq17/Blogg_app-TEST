15d625dee800a0c2afc946752f26018363b75ede
Konrad Pason <konrad.pason17@gmail.com>
2016-06-22 12:56:12 +0200

    Implement adding comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,7 @@ class Article < ActiveRecord::Base
 	validates :title, presence: true
 	validates :body, presence: true
 	belongs_to :user
+	has_many :comments
 
 	default_scope { order(created_at: :desc) }
 end
